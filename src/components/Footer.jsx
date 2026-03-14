e35ee9edc8e4e1e1c55677bd7e958f37879be4c2
ABODUNDE OLUWAFERANMI EMMANUEL <abodundeoluwaferanmi@gmail.com>
2026-03-14 17:49:14 +0100

    Footer Implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,31 @@
+import { footerLinks } from "../constants";
+
 function Footer() {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or a retailer near you. Or call
+          1223 2345 44
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2026 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
